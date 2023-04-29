f254a6bfcc8eef9dc7720eaff8b3c32962351030
billi.nguyen <billi.nguyen@linecorp.com>
2023-04-30 03:12:24 +0900

    subject class implementation

--- src/Subject.js
+++ src/Subject.js
@@ -1,5 +1,26 @@
 class Subject {
-  // TO DO
+	constructor() {
+		this.unsubscribeAll()
+	}
+
+	subscribe(obs) {
+		if (obs != null) this.observers.push(obs);
+	}
+
+	unsubscribe(obs) {
+		if (obs != null) {
+			this.observers = this.observers.filter(x => x!== obs);
+		}
+	}
+
+	unsubscribeAll() {
+		this.observers = []
+	}
+	
+	notifySubscribers(source, ...others) {
+		for (let obs of this.observers)
+			if (obs != null) obs.update(source, ...others);
+	}
 }
 
 export { Subject };
