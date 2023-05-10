764852ecf111f2d57a34cc28d99b1480522c3803
billi.nguyen <billi.nguyen@linecorp.com>
2023-05-11 04:38:54 +0900

    implemented attacker class

---
+++ src/Attacker.js
@@ -0,0 +1,81 @@
+import { ATTACKER_HEIGTH, ATTACKER_WIDTH, WINDOW_HEIGHT, WINDOW_WIDTH } from './Constants.js';
+import { Subject } from './Subject';
+import { loadAndScale, loadMultiAndScale } from './Util.js';
+
+// Assets
+import smokeImg from '../assets/rockets/smoke.png';
+
+class Attacker extends Subject  {
+	constructor(speedMin, speedMax) {
+        super();
+        this.rocket = new Sprite(random(0,WINDOW_WIDTH), 0, ATTACKER_WIDTH, ATTACKER_HEIGTH, 'kinematic');
+        loadMultiAndScale(10, "assets/explosions/expl", ".png").then((loadedArr)=>{
+            this.rocket.addAnimation("explosion", ...loadedArr);
+            this.rocket.animation.noLoop();
+        });
+        loadAndScale(smokeImg, ATTACKER_WIDTH, ATTACKER_HEIGTH).then((loadedImg) => {
+            this.rocket.addAnimation("rocket", loadedImg);
+            this.rocket.changeAnimation("rocket");
+        });
+        this.speed = random(speedMin, speedMax);
+        this.angle = random(45, 160);
+        this.rocket.rotation = this.angle;
+        this.rocket.direction = this.angle;
+        this.rocket.autoDraw = false;
+    }
+
+    rocketLaunch() {
+        this.rocket.speed = this.speed;
+    }
+
+    rocketExploded() {
+        this.rocket.changeAnimation("explosion").then(()=>{
+            this.rocketReset();
+        });
+    }
+
+    rocketReset(isInit) {
+        if (isInit || this.rocket.y > WINDOW_HEIGHT+10 || (this.rocket.animation.name == "explosion" && this.rocket.animation.frame == 9)) {
+            this.rocket.changeAnimation("rocket");
+            this.rocket.x = random(0,width);
+            this.rocket.y = 0;
+            this.angle = random(45, 160);
+            this.rocket.rotation = this.angle;
+            this.rocket.direction = this.angle;
+            this.rocket.speed = 0;
+        }
+    }
+
+    getSprite() {
+        return this.rocket;
+    }
+
+    isHit(objArr) {
+        for (let i = 0; i<objArr.length; i++) {
+            if (this.rocket.collides(objArr[i].getSprite())) return i;
+            if (this.rocket.colliding(objArr[i].getSprite())) return i;
+            if (this.rocket.collided(objArr[i].getSprite())) return i;
+        }
+        return -1;
+    }
+
+    isHitMulti(objArr) {
+        let res = [];
+        for (let i = 0; i<objArr.length; i++) {
+            if (this.rocket.collides(objArr[i].getSprite())) res.push(i);
+            // if (this.rocket.colliding(objArr[i].getSprite())) return i;
+            // if (this.rocket.collided(objArr[i].getSprite())) return i;
+        }
+        return res;
+    }
+
+	draw() {
+        this.rocket.autoDraw = true;
+    }
+
+    unDraw() {
+        this.rocket.autoDraw = false;
+    }
+}
+
+export { Attacker };
